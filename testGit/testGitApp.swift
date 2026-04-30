01240bd345947c419df4b8acbb9e70bcfe535486
LiecardoAntonio <liecardoantonio24@gmail.com>
2026-04-30 13:00:26 +0700

    initial commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by Liecardo on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
